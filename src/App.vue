8fefb27845959f3d7a0fb82904ca177e30b5bd70
Kirk Sandstrom <kirksandstrom@gmail.com>
2023-06-16 21:00:04 -0400

    added basic scaffolding for accordion

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AccordionItem from "./components/AccordionItem.vue";
+
+export default {
+  components: {
+    AccordionItem,
+  },
+  data() {
+    return {
+      message: "Hello world!",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <div class="accordion">
+    <AccordionItem></AccordionItem>
+  </div>
+</template>
---
+++ src/components/AccordionItem.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      headingMessage: "Click this to see something cool",
+      bodyMessage: "Something cool",
+      bodyDisplayed: false,
+    };
+  },
+  methods: {
+    toggleBodyDisplay() {
+      this.bodyDisplayed
+        ? (this.bodyDisplayed = false)
+        : (this.bodyDisplayed = true);
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="accordion__heading" @click="toggleBodyDisplay">
+    <h2>{{ headingMessage }}</h2>
+  </div>
+  <div
+    class="accordion__body"
+    :class="{
+      'accordion__body--displayed-yes': bodyDisplayed,
+    }"
+  >
+    <p>{{ bodyMessage }}</p>
+  </div>
+</template>
